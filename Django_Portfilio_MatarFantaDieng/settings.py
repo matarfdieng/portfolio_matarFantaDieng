bb5dd68123af6817c2ca8819bd8129c5a3ee46f7
matarfdieng <95430900+matarfdieng@users.noreply.github.com>
2026-07-20 21:52:42 +0000

    Update settings.py

--- Django_Portfilio_MatarFantaDieng/settings.py
+++ Django_Portfilio_MatarFantaDieng/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-85t%)xy5tujn&^s3t2(9$c261e^%o)_9d(sk0d78ku#i^vxhj0'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
